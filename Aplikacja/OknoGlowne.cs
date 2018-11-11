cd0188e9e3cd49a053e8dcdf16cad0c4e797a0b6
mmorawa <32575904+mmorawa@users.noreply.github.com>
2018-11-11 16:42:01 +0100

    Zmiana okna głównego aplikacji

--- Aplikacja/OknoGlowne.cs
+++ Aplikacja/OknoGlowne.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace Aplikacja
 {
-    public partial class Form1 : Form
+    public partial class OknoGlowne : Form
     {
-        public Form1()
+        public OknoGlowne()
         {
             InitializeComponent();
             //test gitea
