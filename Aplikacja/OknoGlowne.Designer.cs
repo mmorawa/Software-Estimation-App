b00bbd3077201b702972a7d17bab5754e7fcac1c
mmorawa <32575904+mmorawa@users.noreply.github.com>
2018-11-11 21:06:43 +0100

    Pasek Menu

--- Aplikacja/OknoGlowne.Designer.cs
+++ Aplikacja/OknoGlowne.Designer.cs
@@ -28,20 +28,149 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.PasekMenu = new System.Windows.Forms.MenuStrip();
+            this.plikToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.noweOszacowanieToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.zapiszToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.opcjeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.oProgramieToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.wyjścieToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dokumentacjaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.oProgramieToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.kalibracjaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.raportyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.PasekMenu.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // PasekMenu
+            // 
+            this.PasekMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.plikToolStripMenuItem,
+            this.raportyToolStripMenuItem,
+            this.opcjeToolStripMenuItem,
+            this.oProgramieToolStripMenuItem});
+            this.PasekMenu.Location = new System.Drawing.Point(0, 0);
+            this.PasekMenu.Name = "PasekMenu";
+            this.PasekMenu.Size = new System.Drawing.Size(1321, 24);
+            this.PasekMenu.TabIndex = 0;
+            this.PasekMenu.Text = "Pasek Menu";
+            // 
+            // plikToolStripMenuItem
+            // 
+            this.plikToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.noweOszacowanieToolStripMenuItem,
+            this.toolStripMenuItem1,
+            this.zapiszToolStripMenuItem,
+            this.toolStripMenuItem2,
+            this.wyjścieToolStripMenuItem});
+            this.plikToolStripMenuItem.Name = "plikToolStripMenuItem";
+            this.plikToolStripMenuItem.Size = new System.Drawing.Size(38, 20);
+            this.plikToolStripMenuItem.Text = "&Plik";
+            // 
+            // noweOszacowanieToolStripMenuItem
+            // 
+            this.noweOszacowanieToolStripMenuItem.Name = "noweOszacowanieToolStripMenuItem";
+            this.noweOszacowanieToolStripMenuItem.Size = new System.Drawing.Size(202, 22);
+            this.noweOszacowanieToolStripMenuItem.Text = "&Nowe oszacowanie";
+            // 
+            // zapiszToolStripMenuItem
+            // 
+            this.zapiszToolStripMenuItem.Name = "zapiszToolStripMenuItem";
+            this.zapiszToolStripMenuItem.Size = new System.Drawing.Size(202, 22);
+            this.zapiszToolStripMenuItem.Text = "&Zapisz oszacowanie";
+            // 
+            // opcjeToolStripMenuItem
+            // 
+            this.opcjeToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.kalibracjaToolStripMenuItem});
+            this.opcjeToolStripMenuItem.Name = "opcjeToolStripMenuItem";
+            this.opcjeToolStripMenuItem.Size = new System.Drawing.Size(50, 20);
+            this.opcjeToolStripMenuItem.Text = "&Opcje";
+            // 
+            // oProgramieToolStripMenuItem
+            // 
+            this.oProgramieToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.dokumentacjaToolStripMenuItem,
+            this.oProgramieToolStripMenuItem1});
+            this.oProgramieToolStripMenuItem.Name = "oProgramieToolStripMenuItem";
+            this.oProgramieToolStripMenuItem.Size = new System.Drawing.Size(57, 20);
+            this.oProgramieToolStripMenuItem.Text = "Pomo&c";
+            // 
+            // wyjścieToolStripMenuItem
+            // 
+            this.wyjścieToolStripMenuItem.Name = "wyjścieToolStripMenuItem";
+            this.wyjścieToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.wyjścieToolStripMenuItem.Text = "&Wyjście";
+            // 
+            // dokumentacjaToolStripMenuItem
+            // 
+            this.dokumentacjaToolStripMenuItem.Name = "dokumentacjaToolStripMenuItem";
+            this.dokumentacjaToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.dokumentacjaToolStripMenuItem.Text = "&Dokumentacja";
+            // 
+            // oProgramieToolStripMenuItem1
+            // 
+            this.oProgramieToolStripMenuItem1.Name = "oProgramieToolStripMenuItem1";
+            this.oProgramieToolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
+            this.oProgramieToolStripMenuItem1.Text = "&O programie";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(202, 22);
+            this.toolStripMenuItem1.Text = "&Otwórz oszacowanie";
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(202, 22);
+            this.toolStripMenuItem2.Text = "Z&apisz oszacowanie jako";
+            // 
+            // kalibracjaToolStripMenuItem
+            // 
+            this.kalibracjaToolStripMenuItem.Name = "kalibracjaToolStripMenuItem";
+            this.kalibracjaToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.kalibracjaToolStripMenuItem.Text = "&Kalibracja";
+            // 
+            // raportyToolStripMenuItem
+            // 
+            this.raportyToolStripMenuItem.Name = "raportyToolStripMenuItem";
+            this.raportyToolStripMenuItem.Size = new System.Drawing.Size(60, 20);
+            this.raportyToolStripMenuItem.Text = "&Raporty";
             // 
             // OknoGlowne
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1321, 784);
+            this.Controls.Add(this.PasekMenu);
+            this.MainMenuStrip = this.PasekMenu;
             this.Name = "OknoGlowne";
             this.Text = "Aplikacja wspomagająca szacowanie pracochłonności wytwarzania oprogramowania";
+            this.PasekMenu.ResumeLayout(false);
+            this.PasekMenu.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip PasekMenu;
+        private System.Windows.Forms.ToolStripMenuItem plikToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem noweOszacowanieToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem zapiszToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem opcjeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem oProgramieToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem wyjścieToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem dokumentacjaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem oProgramieToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem raportyToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem kalibracjaToolStripMenuItem;
     }
 }
 
