a34a9a7dd36b67f61423c06265cc28b086cab38c
mmorawa <32575904+mmorawa@users.noreply.github.com>
2018-11-12 16:06:55 +0100

    Szkielet funkcji Menu. Dane projektu

--- Aplikacja/OknoGlowne.Designer.cs
+++ Aplikacja/OknoGlowne.Designer.cs
@@ -30,18 +30,26 @@
         {
             this.PasekMenu = new System.Windows.Forms.MenuStrip();
             this.plikToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.noweOszacowanieToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.zapiszToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ToolStripMenuNowyProj = new System.Windows.Forms.ToolStripMenuItem();
+            this.ToolStripMenuOtworzProj = new System.Windows.Forms.ToolStripMenuItem();
+            this.ToolStripMenuZapiszProj = new System.Windows.Forms.ToolStripMenuItem();
+            this.ToolStripMenuZapiszJakoProj = new System.Windows.Forms.ToolStripMenuItem();
+            this.ToolStripMenuWyjscie = new System.Windows.Forms.ToolStripMenuItem();
+            this.raportyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.opcjeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.kalibracjaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.przelicznikiToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.oProgramieToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.wyjścieToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.dokumentacjaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.oProgramieToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
-            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
-            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
-            this.kalibracjaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.raportyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.DaneProjektu = new System.Windows.Forms.GroupBox();
+            this.LabelDataRozp = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.button1 = new System.Windows.Forms.Button();
+            this.LabelNazwaProjektu = new System.Windows.Forms.Label();
+            this.TextBoxNazwaProjektu = new System.Windows.Forms.TextBox();
             this.PasekMenu.SuspendLayout();
+            this.DaneProjektu.SuspendLayout();
             this.SuspendLayout();
             // 
             // PasekMenu
@@ -60,34 +68,76 @@
             // plikToolStripMenuItem
             // 
             this.plikToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.noweOszacowanieToolStripMenuItem,
-            this.toolStripMenuItem1,
-            this.zapiszToolStripMenuItem,
-            this.toolStripMenuItem2,
-            this.wyjścieToolStripMenuItem});
+            this.ToolStripMenuNowyProj,
+            this.ToolStripMenuOtworzProj,
+            this.ToolStripMenuZapiszProj,
+            this.ToolStripMenuZapiszJakoProj,
+            this.ToolStripMenuWyjscie});
             this.plikToolStripMenuItem.Name = "plikToolStripMenuItem";
             this.plikToolStripMenuItem.Size = new System.Drawing.Size(38, 20);
             this.plikToolStripMenuItem.Text = "&Plik";
             // 
-            // noweOszacowanieToolStripMenuItem
+            // ToolStripMenuNowyProj
             // 
-            this.noweOszacowanieToolStripMenuItem.Name = "noweOszacowanieToolStripMenuItem";
-            this.noweOszacowanieToolStripMenuItem.Size = new System.Drawing.Size(202, 22);
-            this.noweOszacowanieToolStripMenuItem.Text = "&Nowe oszacowanie";
+            this.ToolStripMenuNowyProj.Name = "ToolStripMenuNowyProj";
+            this.ToolStripMenuNowyProj.Size = new System.Drawing.Size(180, 22);
+            this.ToolStripMenuNowyProj.Text = "&Nowy projekt";
+            this.ToolStripMenuNowyProj.Click += new System.EventHandler(this.ToolStripMenuNowyProj_Click);
             // 
-            // zapiszToolStripMenuItem
+            // ToolStripMenuOtworzProj
             // 
-            this.zapiszToolStripMenuItem.Name = "zapiszToolStripMenuItem";
-            this.zapiszToolStripMenuItem.Size = new System.Drawing.Size(202, 22);
-            this.zapiszToolStripMenuItem.Text = "&Zapisz oszacowanie";
+            this.ToolStripMenuOtworzProj.Name = "ToolStripMenuOtworzProj";
+            this.ToolStripMenuOtworzProj.Size = new System.Drawing.Size(180, 22);
+            this.ToolStripMenuOtworzProj.Text = "&Otwórz projekt";
+            this.ToolStripMenuOtworzProj.Click += new System.EventHandler(this.ToolStripMenuOtworzProj_Click);
+            // 
+            // ToolStripMenuZapiszProj
+            // 
+            this.ToolStripMenuZapiszProj.Name = "ToolStripMenuZapiszProj";
+            this.ToolStripMenuZapiszProj.Size = new System.Drawing.Size(180, 22);
+            this.ToolStripMenuZapiszProj.Text = "&Zapisz projekt";
+            this.ToolStripMenuZapiszProj.Click += new System.EventHandler(this.ToolStripMenuZapiszProj_Click);
+            // 
+            // ToolStripMenuZapiszJakoProj
+            // 
+            this.ToolStripMenuZapiszJakoProj.Name = "ToolStripMenuZapiszJakoProj";
+            this.ToolStripMenuZapiszJakoProj.Size = new System.Drawing.Size(180, 22);
+            this.ToolStripMenuZapiszJakoProj.Text = "Z&apisz projekt jako";
+            this.ToolStripMenuZapiszJakoProj.Click += new System.EventHandler(this.ToolStripMenuZapiszJakoProj_Click);
+            // 
+            // ToolStripMenuWyjscie
+            // 
+            this.ToolStripMenuWyjscie.Name = "ToolStripMenuWyjscie";
+            this.ToolStripMenuWyjscie.Size = new System.Drawing.Size(180, 22);
+            this.ToolStripMenuWyjscie.Text = "&Wyjście";
+            this.ToolStripMenuWyjscie.Click += new System.EventHandler(this.ToolStripMenuWyjscie_Click);
+            // 
+            // raportyToolStripMenuItem
+            // 
+            this.raportyToolStripMenuItem.Name = "raportyToolStripMenuItem";
+            this.raportyToolStripMenuItem.Size = new System.Drawing.Size(60, 20);
+            this.raportyToolStripMenuItem.Text = "&Raporty";
             // 
             // opcjeToolStripMenuItem
             // 
             this.opcjeToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.kalibracjaToolStripMenuItem});
+            this.kalibracjaToolStripMenuItem,
+            this.przelicznikiToolStripMenuItem});
             this.opcjeToolStripMenuItem.Name = "opcjeToolStripMenuItem";
-            this.opcjeToolStripMenuItem.Size = new System.Drawing.Size(50, 20);
-            this.opcjeToolStripMenuItem.Text = "&Opcje";
+            this.opcjeToolStripMenuItem.Size = new System.Drawing.Size(76, 20);
+            this.opcjeToolStripMenuItem.Text = "&Ustawienia";
+            // 
+            // kalibracjaToolStripMenuItem
+            // 
+            this.kalibracjaToolStripMenuItem.Name = "kalibracjaToolStripMenuItem";
+            this.kalibracjaToolStripMenuItem.Size = new System.Drawing.Size(132, 22);
+            this.kalibracjaToolStripMenuItem.Text = "&Kalibracja";
+            // 
+            // przelicznikiToolStripMenuItem
+            // 
+            this.przelicznikiToolStripMenuItem.Name = "przelicznikiToolStripMenuItem";
+            this.przelicznikiToolStripMenuItem.Size = new System.Drawing.Size(132, 22);
+            this.przelicznikiToolStripMenuItem.Text = "&Przeliczniki";
             // 
             // oProgramieToolStripMenuItem
             // 
@@ -98,59 +148,88 @@
             this.oProgramieToolStripMenuItem.Size = new System.Drawing.Size(57, 20);
             this.oProgramieToolStripMenuItem.Text = "Pomo&c";
             // 
-            // wyjścieToolStripMenuItem
-            // 
-            this.wyjścieToolStripMenuItem.Name = "wyjścieToolStripMenuItem";
-            this.wyjścieToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
-            this.wyjścieToolStripMenuItem.Text = "&Wyjście";
-            // 
             // dokumentacjaToolStripMenuItem
             // 
             this.dokumentacjaToolStripMenuItem.Name = "dokumentacjaToolStripMenuItem";
-            this.dokumentacjaToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.dokumentacjaToolStripMenuItem.Size = new System.Drawing.Size(151, 22);
             this.dokumentacjaToolStripMenuItem.Text = "&Dokumentacja";
             // 
             // oProgramieToolStripMenuItem1
             // 
             this.oProgramieToolStripMenuItem1.Name = "oProgramieToolStripMenuItem1";
-            this.oProgramieToolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
+            this.oProgramieToolStripMenuItem1.Size = new System.Drawing.Size(151, 22);
             this.oProgramieToolStripMenuItem1.Text = "&O programie";
             // 
-            // toolStripMenuItem1
+            // DaneProjektu
             // 
-            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
-            this.toolStripMenuItem1.Size = new System.Drawing.Size(202, 22);
-            this.toolStripMenuItem1.Text = "&Otwórz oszacowanie";
+            this.DaneProjektu.Controls.Add(this.LabelDataRozp);
+            this.DaneProjektu.Controls.Add(this.dateTimePicker1);
+            this.DaneProjektu.Controls.Add(this.button1);
+            this.DaneProjektu.Controls.Add(this.LabelNazwaProjektu);
+            this.DaneProjektu.Controls.Add(this.TextBoxNazwaProjektu);
+            this.DaneProjektu.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.DaneProjektu.Location = new System.Drawing.Point(22, 42);
+            this.DaneProjektu.Name = "DaneProjektu";
+            this.DaneProjektu.Size = new System.Drawing.Size(503, 150);
+            this.DaneProjektu.TabIndex = 1;
+            this.DaneProjektu.TabStop = false;
+            this.DaneProjektu.Text = "Dane projektu";
             // 
-            // toolStripMenuItem2
+            // LabelDataRozp
             // 
-            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
-            this.toolStripMenuItem2.Size = new System.Drawing.Size(202, 22);
-            this.toolStripMenuItem2.Text = "Z&apisz oszacowanie jako";
+            this.LabelDataRozp.AutoSize = true;
+            this.LabelDataRozp.Location = new System.Drawing.Point(6, 74);
+            this.LabelDataRozp.Name = "LabelDataRozp";
+            this.LabelDataRozp.Size = new System.Drawing.Size(133, 16);
+            this.LabelDataRozp.TabIndex = 4;
+            this.LabelDataRozp.Text = "Data rozpoczęcia:";
             // 
-            // kalibracjaToolStripMenuItem
+            // dateTimePicker1
             // 
-            this.kalibracjaToolStripMenuItem.Name = "kalibracjaToolStripMenuItem";
-            this.kalibracjaToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
-            this.kalibracjaToolStripMenuItem.Text = "&Kalibracja";
+            this.dateTimePicker1.Location = new System.Drawing.Point(6, 93);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 22);
+            this.dateTimePicker1.TabIndex = 3;
             // 
-            // raportyToolStripMenuItem
+            // button1
             // 
-            this.raportyToolStripMenuItem.Name = "raportyToolStripMenuItem";
-            this.raportyToolStripMenuItem.Size = new System.Drawing.Size(60, 20);
-            this.raportyToolStripMenuItem.Text = "&Raporty";
+            this.button1.Location = new System.Drawing.Point(262, 88);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(173, 27);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Więcej szczegółów";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // LabelNazwaProjektu
+            // 
+            this.LabelNazwaProjektu.AutoSize = true;
+            this.LabelNazwaProjektu.Location = new System.Drawing.Point(6, 30);
+            this.LabelNazwaProjektu.Name = "LabelNazwaProjektu";
+            this.LabelNazwaProjektu.Size = new System.Drawing.Size(118, 16);
+            this.LabelNazwaProjektu.TabIndex = 1;
+            this.LabelNazwaProjektu.Text = "Nazwa projektu:";
+            // 
+            // TextBoxNazwaProjektu
+            // 
+            this.TextBoxNazwaProjektu.Location = new System.Drawing.Point(6, 49);
+            this.TextBoxNazwaProjektu.Name = "TextBoxNazwaProjektu";
+            this.TextBoxNazwaProjektu.Size = new System.Drawing.Size(462, 22);
+            this.TextBoxNazwaProjektu.TabIndex = 0;
             // 
             // OknoGlowne
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1321, 784);
+            this.Controls.Add(this.DaneProjektu);
             this.Controls.Add(this.PasekMenu);
             this.MainMenuStrip = this.PasekMenu;
             this.Name = "OknoGlowne";
             this.Text = "Aplikacja wspomagająca szacowanie pracochłonności wytwarzania oprogramowania";
             this.PasekMenu.ResumeLayout(false);
             this.PasekMenu.PerformLayout();
+            this.DaneProjektu.ResumeLayout(false);
+            this.DaneProjektu.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -160,17 +239,24 @@
 
         private System.Windows.Forms.MenuStrip PasekMenu;
         private System.Windows.Forms.ToolStripMenuItem plikToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem noweOszacowanieToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem zapiszToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ToolStripMenuNowyProj;
+        private System.Windows.Forms.ToolStripMenuItem ToolStripMenuZapiszProj;
         private System.Windows.Forms.ToolStripMenuItem opcjeToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem oProgramieToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem wyjścieToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ToolStripMenuWyjscie;
         private System.Windows.Forms.ToolStripMenuItem dokumentacjaToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem oProgramieToolStripMenuItem1;
-        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
-        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem ToolStripMenuOtworzProj;
+        private System.Windows.Forms.ToolStripMenuItem ToolStripMenuZapiszJakoProj;
         private System.Windows.Forms.ToolStripMenuItem raportyToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem kalibracjaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem przelicznikiToolStripMenuItem;
+        private System.Windows.Forms.GroupBox DaneProjektu;
+        private System.Windows.Forms.Label LabelNazwaProjektu;
+        private System.Windows.Forms.TextBox TextBoxNazwaProjektu;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label LabelDataRozp;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
     }
 }
 
--- Aplikacja/OknoGlowne.cs
+++ Aplikacja/OknoGlowne.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿/*! \file Form1.cs
+\brief Plik Form1.cs zawiera klasę Form1 z głównym oknem aplikacji.
+
+Plik Form1.cs zawiera rdzeń programu czyli główne okno aplikacji wraz z menu, pozwalającym na dokonanie wyboru różnych opcji menadżera zadań takich jak: szyfrowanie/deszyfrowanie bazy danych.
+*/
+
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -8,14 +14,71 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
+//! Przestrzeń nazw Password_Manager obejmuje całą aplikację Menedżera Haseł.
+/*! Password_Manager to przestrzeń nazw, która obejmuje całą aplikację Menedżera Haseł, ze wszystkimi klasami Form. */
 namespace Aplikacja
 {
+    //! Klasa Form1 zawiera główne okno aplikacji.
+    /*! W klasie Form1 znajduje się menu główne aplikacji, przyciski oraz Textboxy z danymi, oraz warstwa logiczna aplikacji */
     public partial class OknoGlowne : Form
     {
+        //! Konstruktor klasy Form1.
+        /*! Inicjalizuje komponenty klasy Form1. */
         public OknoGlowne()
         {
             InitializeComponent();
-            //test gitea
+            
+        }
+
+        /*******************************************************************************
+        *  Metody klasy Form1.
+        ********************************************************************************/
+
+        //! Metoda wywoływana po naciśnięciu przycisku New na pasku Menu.
+        /*! Metoda NewToolStripMenuItem_Click jest wywoływana po naciśnięciu przycisku New na pasku Menu.
+         * Najpierw otwiera nowe okno, gdzie pyta użytkownika o podstawowe dane odnośnie nowej bazy a następnie tworzy pierwszy Panel, w którym użytkownik może zacząć wpisywać swoje dane.
+         */
+        private void ToolStripMenuNowyProj_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        //! Metoda wywoływana po naciśnięciu przycisku Open na pasku Menu.
+        /*! Metoda OpenToolStripMenuItem_Click jest wywoływana po naciśnięciu przycisku Open na pasku Menu.
+         * Odpowiada za wczytanie pliku z bazą haseł a następnie jego odszyfrowanie przy użyciu podanego przez użytkownika klucza.
+         * Następnie generuje ona Panele z Textboxami, do których podstawiane są odszyfrowane dane tekstowe. 
+         */
+        private void ToolStripMenuOtworzProj_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        //! Metoda wywoływana po naciśnięciu przycisku Save na pasku Menu.
+        /*! Metoda SaveToolStripMenuItem_Click jest wywoływana po naciśnięciu przycisku Save na pasku Menu.
+         * Powoduje ona automatyczne zaszyfrowanie wpisów z hasłami, znajdujących w głównym oknie programu bez konieczności podania ścieżki do pliku bazy.
+         */
+        private void ToolStripMenuZapiszProj_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        //! Metoda wywoływana po naciśnięciu przycisku Save As na pasku Menu.
+        /*! Metoda SaveAsToolStripMenuItem_Click jest wywoływana po naciśnięciu przycisku Save As na pasku Menu.
+         * Powoduje ona zamknięcie aktywnej bazy poprzez wywołanie metody CloseDatabase.
+         */
+        private void ToolStripMenuZapiszJakoProj_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        //! Metoda wywoływana po naciśnięciu przycisku Exit na pasku Menu.
+        /*! Metoda ExitToolStripMenuItem_Click jest wywoływana po naciśnięciu przycisku Exit na pasku Menu.
+         * Skutkuje ona zamknięciem aktywnej bazy poprzez wywołanie metody CloseDatabase oraz wyjściem z programu.
+         */
+        private void ToolStripMenuWyjscie_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
         }
     }
 }
